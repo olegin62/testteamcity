75af636f8a3ca0fcb8c784269755a96ee9117bb7
o.veselsky <o.veselsky@upg.kiev.ua>
2013-06-25 15:11:45 +0300

    test

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -24,7 +24,7 @@ namespace WebApp.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Your contact page.vvv";
 
             return View();
         }
