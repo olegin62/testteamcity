b9f2af6526ca8d530d4b79cd05b8de4de28581f5
o.veselsky@upg.kiev.ua <o.veselsky@upg.kiev.ua>
2013-06-27 16:54:35 +0300

    test error

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -24,7 +24,7 @@ namespace WebApp.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page1111";
+            ViewBag.Message = "Your contact page1111"
 
             return View();
         }
